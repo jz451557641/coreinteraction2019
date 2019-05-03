63ca77c5516b38d292a90accf9e14ddc09a516ce
Jizheng Chen <42984495+jz451557641@users.noreply.github.com>
2019-05-02 20:04:05 -0400

    Add files via upload

---
+++ lab/project1/js/script.js
@@ -0,0 +1,31 @@
+$(function() {
+
+  $('[data-scrollmagic]').each(function (index, elem) {
+
+    var scrollMagicController = new ScrollMagic();
+
+
+    var title = $(elem).find('h3'),
+        text = $(elem).find('p'),
+        btn = $(elem).find('a');
+        bot = $(elem).find('botton');
+
+
+    var tl = new TimelineMax({pause: true});
+    tl.add("start")
+      .fromTo(title, 0.4, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+      .fromTo(text, 0.4, { y: '60px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+      .fromTo(btn, 0.4, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+      .fromTo(bot, 0.4, { y: '120px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+
+
+    var scene = new ScrollScene({
+      triggerElement: elem,
+      offset: 0
+    })
+    .setTween(tl)
+    .addTo(scrollMagicController);
+
+
+  });
+});
