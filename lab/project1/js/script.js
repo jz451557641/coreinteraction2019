25ea50113475e744d44c2cc8e3e602ffaadcb536
Jizheng Chen <42984495+jz451557641@users.noreply.github.com>
2019-05-08 18:27:21 -0400

    Add files via upload

--- lab/project1/js/script.js
+++ lab/project1/js/script.js
@@ -8,7 +8,8 @@ $(function() {
     var title = $(elem).find('h3'),
         text = $(elem).find('p'),
         btn = $(elem).find('a');
-        bot = $(elem).find('#botton');
+        bot = $(elem).find('img');
+        niu = $(elem).find('botton')
 
 
     var tl = new TimelineMax({pause: true});
@@ -16,7 +17,8 @@ $(function() {
       .fromTo(title, 0.4, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
       .fromTo(text, 0.4, { y: '60px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
       .fromTo(btn, 0.4, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
-      .fromTo(bot, 0.4, { y: '80px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+      .fromTo(bot, 0.4, { y: '50px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+      .fromTo(niu, 0.4, { y: '100px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
 
 
     var scene = new ScrollScene({
